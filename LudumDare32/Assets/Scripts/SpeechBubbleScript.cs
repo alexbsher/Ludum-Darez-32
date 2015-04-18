aae3c06f3e2ab3859f2005a11b8d3af910eebf3f
James Staley <jstaley@Jamess-MacBook-Pro.local>
2015-04-18 18:32:17 -0400

    Speech Bubble Pre-fab and script"

---
+++ LudumDare32/Assets/Scripts/SpeechBubbleScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpeechBubbleScript : MonoBehaviour {
+
+	public Canvas speechBubbleCanvas;
+	public Text speechBubbleText; 
+	public RawImage speechBubbleImage;
+
+	// Use this for initialization
+	void Start () {
+		speechBubbleText.text = "OH HEY THERE BABY";
+		speechBubbleCanvas.transform.LookAt (Camera.main.transform.localPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		speechBubbleCanvas.transform.LookAt (Camera.main.transform.localPosition);
+//		speechBubbleCanvas.enabled = false; 
+	}
+
+	public void hide () {
+		speechBubbleCanvas.enabled = false; 
+	}
+
+	public void show () {
+		speechBubbleCanvas.enabled = false; 
+	}
+
+	public void setText(string text) {
+		speechBubbleText.text = text;
+	}
+}
