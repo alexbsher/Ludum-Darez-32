10da7f275c22c19a697cf5fc4cb47bf5fcbfa49a
Andrew Sutliff <ajs10904@gmail.com>
2015-04-18 10:47:31 -0400

    Scene basic whitebox, character movement script

---
+++ LudumDare32/Assets/Scripts/CharMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharMovement : MonoBehaviour {
+
+	Vector3 InputVector;
+	Vector3 MovementVector;
+	float MovementSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		InputVector = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+
+		InputVector.Normalize ();
+
+		MovementVector = Vector3.Lerp(MovementVector, InputVector, 0.1f);
+
+		transform.localPosition += MovementVector * MovementSpeed;
+	}
+}
