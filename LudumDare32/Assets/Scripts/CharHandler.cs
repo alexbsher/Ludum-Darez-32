621f2960fcd818dab123cf7ec30735c9640ff77c
Andrew Sutliff <ajs10904@gmail.com>
2015-04-18 14:58:59 -0400

    Added fleeing behavior and punching

---
+++ LudumDare32/Assets/Scripts/CharHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharHandler : MonoBehaviour {
+	
+	public static CharHandler Instance;
+	public List<CharObject> AllChars;
+	
+	public void Awake()
+	{
+		Instance = this;
+		AllChars = new List<CharObject>();
+	}	
+	
+	public CharObject[] GetAllChars()
+	{
+		return AllChars.ToArray();
+	}
+	
+	public void RegisterChar(CharObject newChar)
+	{
+		AllChars.Add (newChar);
+	}
+	
+	public void LoseChar(CharObject newChar)
+	{
+		AllChars.Remove(newChar);
+	}
+}
---
+++ LudumDare32/Assets/Scripts/CharObject.cs
@@ -0,0 +1,279 @@
+using UnityEngine;
+using System.Collections;
+
+public class CharObject : MonoBehaviour {
+
+	Vector3 InputVector;
+	Vector3 MovementVector;
+	Vector3 LookVector;
+	float MovementSpeed;
+	float nextFire;
+	float SpeedMultiplier;
+
+	public float fireRate;
+	public GameObject projectile;
+	public Transform projectileSpawn;
+	public float BaseSpeed = 0.1f;
+	public Transform LookTarget = null;
+	public NavMeshAgent MyNavGhost = null;
+	
+	public GameObject HitParticle = null;
+
+	float xSeed;
+	float zSeed;
+
+	public enum NPCModes
+	{
+		PLAYER,
+		WANDER,
+		FOLLOW,
+		ATTACK,
+		FLEE,
+		DEMON
+	}
+	public NPCModes NPCMode = NPCModes.WANDER;
+
+	// Use this for initialization
+	void Start () {
+		
+		CharHandler.Instance.RegisterChar(this);
+	
+		LookVector = transform.forward;
+		MovementSpeed = BaseSpeed;
+		xSeed = Random.value * 1000.0f;
+		zSeed = Random.value * 1000.0f;
+		SpeedMultiplier = 1;
+		
+		if (MyNavGhost != null)
+			MyNavGhost.transform.parent = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+		CharObject target;
+		switch (NPCMode)
+		{
+			case NPCModes.PLAYER:
+				InputVector = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+				LookTarget = null;
+			break;
+			
+			case NPCModes.WANDER:
+				SpeedMultiplier = 0.25f;
+				
+				InputVector = Vector3.zero;
+				InputVector.x = Mathf.PerlinNoise(Time.time/5 + xSeed,0) * 2 - 1;
+				InputVector.z = Mathf.PerlinNoise(0, Time.time/5 + zSeed) * 2 - 1;
+				
+				LookTarget = null;
+			break;
+
+			case NPCModes.FOLLOW:
+				target = null;
+				foreach(CharObject c in CharHandler.Instance.GetAllChars())
+				{
+					if (c.NPCMode == NPCModes.PLAYER)
+					{
+						if (target == null)
+							target = c;
+						else if ((target.transform.position - transform.position).magnitude > (c.transform.position - transform.position).magnitude)
+							target = c;
+					}
+				}
+				if (target != null)
+				{
+					MyNavGhost.destination = target.transform.position;
+					LookTarget = target.transform;
+				}
+				
+				InputVector = (MyNavGhost.transform.position - transform.position);
+				InputVector.y = 0;
+				
+				if (InputVector.magnitude < 2)
+					InputVector = Vector3.zero;
+				
+				InputVector.Normalize();
+			break;
+	
+			case NPCModes.FLEE:
+			
+				target = null;
+				foreach(CharObject c in CharHandler.Instance.GetAllChars())
+				{
+					if (c.NPCMode == NPCModes.PLAYER)
+					{
+						if (target == null)
+							target = c;
+						else if ((target.transform.position - transform.position).magnitude > (c.transform.position - transform.position).magnitude)
+							target = c;
+					}
+				}
+
+				if (target != null)
+				{
+					InputVector = (transform.position - target.transform.position);
+					InputVector.y = 0;
+				}
+				
+				LookTarget = null;
+				
+				if (InputVector.magnitude > 10)
+					InputVector = Vector3.zero;
+				
+				InputVector.Normalize();
+			break;
+			
+			case NPCModes.ATTACK:
+					target = null;
+					foreach(CharObject c in CharHandler.Instance.GetAllChars())
+					{
+						if (c.NPCMode == NPCModes.DEMON)
+						{
+							if (target == null)
+								target = c;
+							else if ((target.transform.position - transform.position).magnitude > (c.transform.position - transform.position).magnitude)
+									target = c;
+						}
+					}
+					if (target != null)
+					{
+						MyNavGhost.destination = target.transform.position;
+						LookTarget = target.transform;
+					}
+						
+					InputVector = (MyNavGhost.transform.position - transform.position);
+					InputVector.y = 0;
+					
+					if (InputVector.magnitude < 2)
+						InputVector = Vector3.zero;
+					
+					InputVector.Normalize();
+			break;
+			
+			case NPCModes.DEMON:
+				target = null;
+				foreach(CharObject c in CharHandler.Instance.GetAllChars())
+				{
+					if (c.NPCMode != NPCModes.DEMON)
+					{
+						if (target == null)
+							target = c;
+						else if ((target.transform.position - transform.position).magnitude > (c.transform.position - transform.position).magnitude)
+							target = c;
+					}
+				}
+				if (target != null)
+					MyNavGhost.destination = target.transform.position;
+				
+				InputVector = (MyNavGhost.transform.position - transform.position);
+				InputVector.y = 0;
+				
+				if (InputVector.magnitude < 1)
+					InputVector = Vector3.zero;
+				
+				InputVector.Normalize();
+			break;
+			
+		default:
+				
+			break;
+		}
+
+		//--------------;
+
+		RaycastHit hit;
+		Vector3 adjustedInputVector = InputVector;
+
+		if (Physics.SphereCast(transform.position,0.5f,InputVector,out hit))
+		{
+			Vector3 temp = InputVector;
+			if (hit.collider.gameObject.layer != LayerMask.NameToLayer("Bullets"))
+				temp += hit.normal*Mathf.Max(0,Vector3.Dot(temp.normalized,-hit.normal))*temp.magnitude/hit.distance;
+			
+			adjustedInputVector = temp;
+		}
+		
+		//adjustedInputVector = Vector3.Slerp(adjustedInputVector,InputVector,0.1f);
+		
+		//------------;
+		adjustedInputVector.y = 0;
+		adjustedInputVector.Normalize ();
+		
+		MovementSpeed = Mathf.Lerp(MovementSpeed, BaseSpeed * SpeedMultiplier, 0.1f);
+		adjustedInputVector *= MovementSpeed;
+
+		MovementVector = Vector3.Lerp(MovementVector, adjustedInputVector, 0.1f);
+
+		transform.localPosition += MovementVector;
+
+		if (LookTarget != null)
+			LookVector = Vector3.Slerp (LookVector, (LookTarget.position - transform.position).normalized, 0.1f);
+		else {
+			if (MovementVector.magnitude > 0)
+				LookVector = Vector3.Slerp (LookVector, InputVector, 0.1f);
+		}
+		
+		LookVector.y = 0;
+
+		transform.localRotation = Quaternion.LookRotation (LookVector, Vector3.up);
+		
+//		if (Input.GetKeyDown(KeyCode.Space))
+//			AddImpact(transform.forward * -1);
+		
+		if (NPCMode == NPCModes.PLAYER)
+		{
+			if (Input.GetButtonDown("Fire1"))
+				Bonk ();
+			if (Input.GetButtonDown("Fire2"))
+				Splash ();
+		}
+	
+	}
+	
+	
+	public void Bonk()
+	{
+		RaycastHit[] hits = Physics.SphereCastAll(transform.position,0.5f,transform.forward);
+		
+		if (hits != null && hits.Length > 0)
+		{
+			foreach(RaycastHit hit in hits)
+			{
+				if (hit.collider != GetComponent<Collider>())
+					if (hit.distance <= 2)
+						if (hit.collider.gameObject.GetComponent<CharObject>() != null)
+							hit.collider.gameObject.GetComponent<CharObject>().AddImpact(transform.forward);
+			}
+		}
+	}
+	
+	public void Splash()
+	{
+		if (Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (projectile, projectileSpawn.position, projectileSpawn.rotation);
+		}
+	}
+
+	public void AddImpact(Vector3 forceVector)
+	{
+		MovementVector += forceVector;
+	}
+	
+	public void OnCollisionEnter(Collision collision)
+	{
+		Vector3 temp = MovementVector;
+		if (collision.gameObject.layer != LayerMask.NameToLayer("Bullets"))
+			foreach(ContactPoint point in collision.contacts)
+			{		 
+				temp += point.normal*Mathf.Max(0,Vector3.Dot(temp.normalized,-point.normal))*temp.magnitude;
+			}
+		temp.y = 0;
+		MovementVector = temp;
+	}
+
+
+}
