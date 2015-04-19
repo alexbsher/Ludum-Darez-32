29d0a2f318d78d22eaf0fad37042221a62986afe
alexbsher <alexbsher@gmail.com>
2015-04-19 16:44:11 -0400

    DemonSpawner Logic

---
+++ LudumDare32/Assets/Scripts/DemonSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemonSpawner : MonoBehaviour {
+
+	public GameObject demon;
+	public float spawnDelay;
+	private float timeSinceLast;
+	public static DemonSpawner Instance;
+
+	// Use this for initialization
+	void Start () {
+		Instance = this;
+	}
+
+	public void spawnDemon(Transform spawnPoint) {
+		Instantiate (demon, spawnPoint.position, spawnPoint.rotation);
+	}
+}
--- LudumDare32/Assets/Scripts/GameHandler.cs
+++ LudumDare32/Assets/Scripts/GameHandler.cs
@@ -6,15 +6,20 @@ public class GameHandler : MonoBehaviour {
 	public bool isRapture = false;
 	private int score = 0;
 	private float time = 0;
+	private float lastSpawnTime = 0;
+	private float spawnRate = 5.0f;
+
 
 	private float raptureTime = 15.0f;
 	public static GameHandler Instance;
+
+	public Transform[] spawnPoints = new Transform[4];
 
 	void Update() {
 
 		time += Time.deltaTime;
 		if (time > raptureTime) {
-			setRapture(true);
+			handleRapture();
 		}
 
 	}
@@ -37,6 +42,20 @@ public class GameHandler : MonoBehaviour {
 
 	public void changeScore(int scoreChange) {
 		score += scoreChange;
+	}
+
+	public void handleRapture() {
+
+		if (!isRapture) {
+			setRapture (true);
+		}
+
+		if (time - lastSpawnTime > spawnRate) {
+			int spawnIdx = Random.Range(0, spawnPoints.Length);
+			DemonSpawner.Instance.spawnDemon(spawnPoints[spawnIdx]);
+			lastSpawnTime = time;
+		}
+
 	}
 		
 }
