a4a81a18f4abfcb0c71e43cbbd9141c6bd211bf8
Tom Bescherer <tbescherer@gmail.com>
2015-04-18 22:16:24 -0400

    Re-added countdown functionality

--- LudumDare32/Assets/Scripts/TimerGUIScript.cs
+++ LudumDare32/Assets/Scripts/TimerGUIScript.cs
@@ -7,10 +7,6 @@ public class TimerGUIScript : MonoBehaviour {
 	public ClockScript clock;
 	void Update () {
 		int thetime = 180 - ((int)clock.clocktimer * -1);
-		timeText.text = thetime.ToString() + " seconds until the Rapture!";
-	if(thetime <= 0) 
-		{
-			timeText.text = "Oh no it's the rapture!";
-		}
+		gameObject.GetComponent<TextMesh>().text = thetime.ToString();
 	}
 }
