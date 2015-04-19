4ad607580dae973fcceac827a1e3eee12f638602
Tom Bescherer <tbescherer@gmail.com>
2015-04-18 20:27:06 -0400

    added tom stuff

---
+++ LudumDare32/Assets/Scripts/FadeScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeScript : MonoBehaviour {
+	public Text theText;
+	public Text theText2;
+	public Text theText3;
+	public float biblequotetime;
+
+	void Start() 
+	{
+		int quoteint;
+		string[] quotes = new string[4];
+		string[] quotestwo = new string[4];
+		quotes[0] = "But of that day and hour knoweth no man, no, not the angels of heaven,";
+		quotes[1] = "The Lord shall descend from heaven, with the voice of the archangel,";
+		quotes[2] = "For as the lightning cometh out of the east, and shineth even unto the west;";
+		quotes[3] = "And they said, Lord, behold, here are two swords. And he said to them,";
+		quotestwo [0] = "but my Father only.";
+		quotestwo [1] = "and the dead in Christ shall rise first.";
+		quotestwo [2] = "so shall also the coming of the Son of man be.";
+		quotestwo [3] = "It is enough.";
+		quoteint = Random.Range (0,4);
+		theText.text = quotes [quoteint];
+		theText2.text = quotestwo [quoteint];
+		//theText.text = quotes(quoteint);
+	}
+
+	void Update() 
+	{
+		if(Input.anyKey){ Application.LoadLevel (1);}
+		biblequotetime += Time.deltaTime;
+		theText.color = Color.Lerp (Color.black, Color.white, .1f * Time.time);
+
+		if (biblequotetime > 5) {
+			theText2.color = Color.Lerp (Color.black, Color.red, .2f * (Time.time - 5));
+			theText3.color = Color.Lerp (Color.black, Color.red, .2f * (Time.time - 5));
+		}
+		if (biblequotetime > 9) {
+			Application.LoadLevel (1);
+		}
+	}
+}
---
+++ LudumDare32/Assets/Scripts/TimerGUIScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerGUIScript : MonoBehaviour {
+	public Text timeText;
+	public ClockScript clock;
+	void Update () {
+		int thetime = 180 - ((int)clock.clocktimer * -1);
+		timeText.text = thetime.ToString() + " seconds until the Rapture!";
+	if(thetime <= 0) 
+		{
+			timeText.text = "Oh no it's the rapture!";
+		}
+	}
+}
