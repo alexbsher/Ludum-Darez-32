1cb54b891706f6436b4e726a01836911927a8f14
colinegge <colinegge@Colin-Egges-MacBook-Pro.local>
2015-04-19 17:41:21 -0400

    giblets

---
+++ LudumDare32/Assets/addForce.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addForce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector3 force = new Vector3 (Random.value * 2 - 1, Random.value * 2 - 1, Random.value * 2 - 1);
+		GetComponent<Rigidbody> ().AddForce (force * 85);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
