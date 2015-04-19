568463e592adf88377e559f569cb67fdd12c5e4a
alexbsher <alexbsher@gmail.com>
2015-04-19 19:03:31 -0400

    TimerTextObject Script.

--- LudumDare32/Assets/Scripts/TimerGUIScript.cs
+++ LudumDare32/Assets/Scripts/TimerGUIScript.cs
@@ -3,10 +3,20 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class TimerGUIScript : MonoBehaviour {
-	public Text timeText;
-	public ClockScript clock;
-	void Update () {
-		int thetime = 180 - ((int)clock.clocktimer * -1);
-		gameObject.GetComponent<TextMesh>().text = thetime.ToString();
+
+	public string text;
+
+	void Start() {
+
+		Activate();
+
+	}
+	
+	void Activate() {
+		gameObject.GetComponent<TextMesh>().text = text;
+	}
+
+	void Deactivate() {
+		gameObject.GetComponent<TextMesh>().text = null;
 	}
 }
